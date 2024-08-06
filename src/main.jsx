909998d93ffe839177e19fdcc4bf5ec409becfde
AngelJosue12 <20210672@uthh.edu.mx>
2024-08-06 14:20:49 -0600

    react

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import App from './App'
+import App from './App.jsx'
 import { AuthProvider } from './Components/Contexts/AuthContexts.jsx';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <AuthProvider>
-  <App />
-</AuthProvider>,
-);
+ReactDOM.render(
+      <AuthProvider>
+        <App />
+      </AuthProvider>,
+    document.getElementById('root')
+  );
